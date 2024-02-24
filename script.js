5e56b8c9caeec803ede9c6ac402648bae7b38086
jyamee <155151271+jyamee@users.noreply.github.com>
2024-02-24 22:05:57 +0300

    changed  class names in css file to match my design

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  let cityElement = searchinput.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
+  let cityElement = document.querySelector("#city");
 
+  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
-function searchcity(city) {
-  let apikey = "89624f36990b08aabeaatob0f874fe47";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
-  console.log(apiUrl);
+function searchCity(city) {
+  let apiKey = "89624f36990b08aabeaatob0f874fe47";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
-function searchsubmit(event) {
+function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchforminput = document.querySelector("#search-form-input");
-  searchcity(searchforminput.value);
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
 }
 
-let searchformElement = document.querySelector("#search-form");
-searchformElement.addEventListener("submit", searchsubmit);
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchcity("Kampala");
+searchCity("Paris");
