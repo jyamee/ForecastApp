cb9d571474bf58fab73986b15d0cb72446a1094a
jyamee <155151271+jyamee@users.noreply.github.com>
2024-02-24 21:31:27 +0300

    added id elements to the html file for functions

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-function handlesearchsubmit(event) {
-  event.preventDefault();
-  let searchinput = document.querySelector("#search-form-input");
-  let cityElement = searchinput.querySelector("#app-city");
-  cityElement.innerHTML = searchinput.value;
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = searchinput.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
-let searchformElement = document.querySelector("search-form");
-searchformElement.addEventListener("submit", handlesearchsubmit);
+function searchcity(city) {
+  let apikey = "89624f36990b08aabeaatob0f874fe47";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function searchsubmit(event) {
+  event.preventDefault();
+  let searchforminput = document.querySelector("#search-form-input");
+  searchcity(searchforminput.value);
+}
+
+let searchformElement = document.querySelector("#search-form");
+searchformElement.addEventListener("submit", searchsubmit);
+
+searchcity("Kampala");
